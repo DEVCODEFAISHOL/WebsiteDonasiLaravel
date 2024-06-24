7338c4ecb3afb165ed34f9275dab5790d3e4fbae
DEVCODEFAISHOL <faishol2561@gmail.com>
2024-06-25 02:43:58 +0700

    Menambahkan migrasi untuk tabel donaturs

---
+++ database/migrations/2024_06_24_170908_create_donaturs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('donaturs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('fundraising_id');
+            $table->string('name');
+            $table->integer('total_amount');
+            $table->string('phone_number');
+            $table->text('notes')->nullable();
+            $table->boolean('is_paid')->default(false);
+            $table->string('proof');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('donaturs');
+    }
+};
