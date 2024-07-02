d6e894f8603132cac5e01469b55ffb177b3d8058
DEVCODEFAISHOL <faishol2561@gmail.com>
2024-07-02 13:14:31 +0700

    📁 Menambahkan struktur direktori Admin dengan subdirektori: categories, donaturs, fundraisers, fundraising_withdrawals, fundraisings, my_withdrawals

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,5 +32,6 @@
                 {{ $slot }}
             </main>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
     </body>
 </html>
